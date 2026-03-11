0540435a24c949cc5623f26910ae1e422455d12f
kyxw007 <kyxw007@gmail.com>
2026-03-11 11:00:53 +0800

    修改字体大小

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,15 +1,19 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
+local font_size = platform.is_mac and 12 or 9.75
+
 -- 根据平台选择字体名称
 local font_family = 'SF Mono' -- 默认（Mac）
-if platform.is_win then
+if platform.is_mac then
+    font_size = platform.is_mac and 12 or 9.75
+elseif platform.is_win then
     font_family = 'Cascadia Code' -- 或者 'Consolas'
+    font_size = 12
 elseif platform.is_linux then
     font_family = 'JetBrains Mono'
+    font_size = 12
 end
-
-local font_size = platform.is_mac and 12 or 9.75
 
 return {
    font = wezterm.font({
